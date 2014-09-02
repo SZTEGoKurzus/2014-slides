961f28c8e38f74e830291b362068e0d972dd685d
Tamás Demeter-Haludka <yorirou@gmail.com>
2014-09-02 19:34:32 +0200

    Slides 01.

---
+++ 01/hellomath.go
@@ -0,0 +1,10 @@
+package hellomath
+
+// Sqrt returns an approximation to the square root of x.
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * z)
+		return z
+	}
+}
